55928bf7b4a9f05789997dc880c6e67788dfb3a7
CHE Yulin <yche@cse.ust.hk>
2019-10-10 22:42:30 +0800

    add the input and output in playground

--- truss-decomp/playground/test_input.cpp
+++ truss-decomp/playground/test_input.cpp
@@ -2,6 +2,23 @@
 // Created by yche on 10/10/19.
 //
 
-int main() {
+#include "util/file_system/file_util.h"
+#include "util/log.h"
+#include "util/program_options/popl.h"
 
+int main(int argc, char *argv[]) {
+
+    using namespace popl;
+    OptionParser op("Allowed options");
+    auto string_option = op.add<Value<std::string>>("f", "file-path", "the graph bin file path");
+
+    op.parse(argc, argv);
+
+    if (string_option->is_set()) {
+        size_t size = file_size(string_option->value(0).c_str());
+        size_t num_edges = size / sizeof(uint32_t) / 2;
+        log_info("File size: %zu", size);
+        log_info("#of Edges: %zu", num_edges);
+        log_info("There are %zu triangles in the input graph.", 0);
+    }
 }
