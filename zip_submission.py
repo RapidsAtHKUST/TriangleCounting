f908d93c63cb97c438e228870f8593b5849cc90d
CHE Yulin <yche@cse.ust.hk>
2019-10-11 21:38:31 +0800

    update script and makefile for submission

--- zip_submission.py
+++ zip_submission.py
@@ -4,4 +4,4 @@ import os
 if __name__ == '__main__':
     date_str = datetime.datetime.now().strftime("%Y-%m-%d-%H:%M")
     print(date_str)
-    os.system('zip -r tc-rapids-{}.zip triangle-counting -x cmake-build-debug -x */CMake*'.format(date_str))
+    os.system('zip -r tc-rapids-{}.zip triangle-counting -x cmake-build-debug -x */CMake* -x *.idea/*'.format(date_str))
