55591d3a8d9c19bb7942daae4e327c94425bd2bf
CHE Yulin <yche@cse.ust.hk>
2019-10-11 11:30:25 +0800

    refine the codes

--- truss-decomp/playground/test_input.cpp
+++ truss-decomp/playground/test_input.cpp
@@ -13,6 +13,7 @@
 #include "util/program_options/popl.h"
 #include "util/sort/parasort_cmp.h"
 #include "util/timer.h"
+
 #include "tc_utils.h"
 #include "primitives.h"
 
@@ -21,37 +22,31 @@ using namespace popl;
 using namespace std::chrono;
 
 template<typename T, typename I>
-void RemoveDuplicates(pair<T, T> *&edge_lst, I &num_edges) {
+T RemoveDuplicates(pair<T, T> *&edge_lst, I &num_edges) {
     using Edge = pair<T, T>;
     Timer timer;
-#pragma omp parallel for
-    for (auto i = 0u; i < num_edges; i++) {
-        if (edge_lst[i].first > edge_lst[i].second) {
-            swap(edge_lst[i].first, edge_lst[i].second);
+    T max_node_id = 0;
+    // Partition.
+#pragma omp parallel
+    {
+#pragma omp for reduction(max: max_node_id)
+        for (auto i = 0u; i < num_edges; i++) {
+            if (edge_lst[i].first > edge_lst[i].second) {
+                swap(edge_lst[i].first, edge_lst[i].second);
+                max_node_id = max(max_node_id, max(edge_lst[i].first, edge_lst[i].second));
+            }
         }
     }
     auto max_omp_threads = omp_get_max_threads();
-//#define BASELINE_SORT
-#ifndef BASELINE_SORT
     parasort(num_edges, edge_lst, [](const Edge &left, const Edge &right) {
         if (left.first == right.first) {
             return left.second < right.second;
         }
         return left.first < right.first;
     }, max_omp_threads);
-#else
-    sort(edge_lst, edge_lst + num_edges,
-         [](const Edge &left, const Edge &right) {
-             if (left.first == right.first) {
-                 return left.second < right.second;
-             }
-             return left.first < right.first;
-         });
-#endif
     log_info("Finish Sort, %.9lfs", timer.elapsed());
 
-//#define NAIVE_REMOVE_DUPLICATE
-#ifndef NAIVE_REMOVE_DUPLICATE
+    // Selection.
     auto *relative_off = (uint32_t *) malloc(sizeof(uint32_t) * num_edges);
     Edge *edge_lst2 = (Edge *) malloc(sizeof(Edge) * num_edges);
     auto histogram = vector<uint32_t>((max_omp_threads + 1) * CACHE_LINE_ENTRY, 0);
@@ -68,33 +63,11 @@ void RemoveDuplicates(pair<T, T> *&edge_lst, I &num_edges) {
             }
         }
     }
-
     edge_lst = edge_lst2;
     num_edges = num_edges - relative_off[num_edges - 1];
-#else
-    vector<Edge> edges2;
 
-    auto last_u = -1;
-    auto last_v = -1;
-    for (auto i = 0u; i < num_edges; i++) {
-        auto edge = edge_lst[i];
-        if (edge.first != last_u) {
-            last_u = edge.first;
-            last_v = edge.second;
-            if (edge.first != edge.second)
-                edges2.emplace_back(edge);
-        } else {
-            if (edge.second != last_v) {
-                last_v = edge.second;
-                if (edge.first != edge.second)
-                    edges2.emplace_back(edge);
-            }
-        }
-    }
-    edge_lst = &edges2.front();
-    num_edges = edges2.size();
-#endif
     log_info("New # of edges: %zu, Elapsed: %.9lfs", num_edges, timer.elapsed());
+    return max_node_id;
 }
 
 int main(int argc, char *argv[]) {
@@ -115,11 +88,10 @@ int main(int argc, char *argv[]) {
         auto file_fd = open(file_name.c_str(), O_RDONLY, S_IRUSR | S_IWUSR);
         Edge *edge_lst =
                 (Edge *) mmap(0, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_POPULATE, file_fd, 0);
-        RemoveDuplicates(edge_lst, num_edges);
+        auto max_node_id = RemoveDuplicates(edge_lst, num_edges);
 
-        int32_t max_node_id = -1;
-        vector<int32_t> deg_lst;
-        vector<uint32_t> off;
+        auto deg_lst = vector<int32_t>(static_cast<uint32_t>(max_node_id + 1));
+        auto off = vector<uint32_t>(static_cast<uint32_t>(max_node_id + 2));;
         vector<int32_t> adj_lst;
         vector<uint32_t> cur_write_off;
         auto load_end = high_resolution_clock::now();
@@ -130,26 +102,6 @@ int main(int argc, char *argv[]) {
 
 #pragma omp parallel
         {
-            // 1st: get the cardinality of degree array
-#pragma omp for reduction(max: max_node_id)
-            for (uint32_t i = 0; i < num_edges; i++) {
-                max_node_id = max(max_node_id, max(edge_lst[i].first, edge_lst[i].second));
-            }
-#pragma omp single nowait
-            {
-                deg_lst = vector<int32_t>(static_cast<uint32_t>(max_node_id + 1));
-
-            }
-#pragma omp single nowait
-            {
-                off = vector<uint32_t>(static_cast<uint32_t>(max_node_id + 2));
-                off[0] = 0;
-            }
-#pragma omp for
-            for (auto i = 0; i < deg_lst.size(); i++) {
-                deg_lst[i] = 0;
-            }
-
             // 2nd: to count grouped neighbors, store in `deg_lst`
 #pragma omp for
             for (uint32_t i = 0; i < num_edges; i++) {
@@ -166,23 +118,17 @@ int main(int argc, char *argv[]) {
                     inc_deg_val = cur_deg_src + 1;
                 } while (!__sync_bool_compare_and_swap(&(deg_lst[dst]), cur_deg_src, inc_deg_val));
             }
-
             // 3rd: compute relative_off and then scatter
 #pragma omp single
             {
                 for (int i = 0; i < deg_lst.size(); i++) {
                     off[i + 1] = off[i] + deg_lst[i];
                 }
+                cur_write_off = off;
             }
-
-#pragma omp single nowait
+#pragma omp single 
             {
                 adj_lst = vector<int32_t>(off[off.size() - 1]);
-            }
-
-#pragma omp single nowait
-            {
-                cur_write_off = off;
             }
 
             // 4th: barrier before we do the computation, and then construct destination vertices in CSR
@@ -192,7 +138,6 @@ int main(int argc, char *argv[]) {
                 log_info("before csr transform time: %.3lf s",
                          duration_cast<milliseconds>(middle - start).count() / 1000.0);
             }
-
 #pragma omp for
             for (uint32_t i = 0; i < num_edges; i++) {
                 auto src = edge_lst[i].first;
