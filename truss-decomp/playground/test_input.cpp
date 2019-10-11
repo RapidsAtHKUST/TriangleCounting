c27452749fb8ead2463086ff340f872ddba856e4
CHE Yulin <yche@cse.ust.hk>
2019-10-11 12:40:59 +0800

    simplify the conversion to CSR

--- truss-decomp/playground/test_input.cpp
+++ truss-decomp/playground/test_input.cpp
@@ -76,8 +76,7 @@ int main(int argc, char *argv[]) {
 
     op.parse(argc, argv);
     using Edge = pair<int32_t, int32_t>;
-    auto load_start = high_resolution_clock::now();
-
+    Timer global_timer;
     if (string_option->is_set()) {
         size_t size = file_size(string_option->value(0).c_str());
         size_t num_edges = size / sizeof(uint32_t) / 2;
@@ -88,94 +87,80 @@ int main(int argc, char *argv[]) {
         auto file_fd = open(file_name.c_str(), O_RDONLY, S_IRUSR | S_IWUSR);
         Edge *edge_lst =
                 (Edge *) mmap(0, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_POPULATE, file_fd, 0);
+
+        // Remove Multi-Edges and Self-Loops.
+        Edge *prev_edge_lst = edge_lst;
+        auto prev_num_edges = num_edges;
         auto max_node_id = RemoveDuplicates(edge_lst, num_edges);
 
-        auto deg_lst = vector<int32_t>(static_cast<uint32_t>(max_node_id + 1));
-        auto off = vector<uint32_t>(static_cast<uint32_t>(max_node_id + 2));;
-        vector<int32_t> adj_lst;
-        vector<uint32_t> cur_write_off;
-        auto load_end = high_resolution_clock::now();
-        log_info("load edge list bin time: %.3lf s",
-                 duration_cast<milliseconds>(load_end - load_start).count() / 1000.0);
+        auto max_omp_threads = omp_get_max_threads();
+        auto histogram = vector<uint32_t>((max_omp_threads + 1) * CACHE_LINE_ENTRY, 0);
+        auto num_vertices = max_node_id + 1;
+        auto deg_lst = vector<int32_t>(static_cast<uint32_t>(num_vertices));
+        auto off = vector<uint32_t>(static_cast<uint32_t>(num_vertices + 1));
+        auto cur_write_off = (uint32_t *) malloc(sizeof(uint32_t) * (num_vertices + 1));
+        int32_t *adj_lst;
+        log_info("load edge list bin time: %.9lf s", global_timer.elapsed());
 
-        auto start = high_resolution_clock::now();
-
-#pragma omp parallel
+        Timer convert_timer;
+#pragma omp parallel num_threads(max_omp_threads)
         {
-            // 2nd: to count grouped neighbors, store in `deg_lst`
+            auto tid = omp_get_thread_num();
+            // Histogram.
 #pragma omp for
             for (uint32_t i = 0; i < num_edges; i++) {
                 // atomic add for edge.first
                 auto src = edge_lst[i].first;
                 auto dst = edge_lst[i].second;
-                int inc_deg_val, cur_deg_src;
-                do {
-                    cur_deg_src = deg_lst[src];
-                    inc_deg_val = cur_deg_src + 1;
-                } while (!__sync_bool_compare_and_swap(&(deg_lst[src]), cur_deg_src, inc_deg_val));
-                do {
-                    cur_deg_src = deg_lst[dst];
-                    inc_deg_val = cur_deg_src + 1;
-                } while (!__sync_bool_compare_and_swap(&(deg_lst[dst]), cur_deg_src, inc_deg_val));
+                __sync_fetch_and_add(&(deg_lst[src]), 1);
+                __sync_fetch_and_add(&(deg_lst[dst]), 1);
             }
-            // 3rd: compute relative_off and then scatter
+            // PrefixSum.
+            {
+                InclusivePrefixSumOMP(histogram, &off.front() + 1, num_vertices, [&deg_lst](uint32_t it) {
+                    return deg_lst[it];
+                }, max_omp_threads);
+                size_t task_num = num_vertices + 1;
+                size_t avg = (task_num + max_omp_threads - 1) / max_omp_threads;
+                auto it_beg = avg * tid;
+                auto it_end = min(avg * (tid + 1), task_num);
+                memcpy(cur_write_off + it_beg, &off.front() + it_beg, sizeof(uint32_t) * (it_end - it_beg));
+            }
 #pragma omp single
             {
-                for (int i = 0; i < deg_lst.size(); i++) {
-                    off[i + 1] = off[i] + deg_lst[i];
-                }
-                cur_write_off = off;
+                adj_lst = (int32_t *) malloc(sizeof(int32_t) * off[off.size() - 1]);
+                log_info("before csr transform time: %.9lf s", convert_timer.elapsed());
             }
-#pragma omp single 
-            {
-                adj_lst = vector<int32_t>(off[off.size() - 1]);
-            }
-
-            // 4th: barrier before we do the computation, and then construct destination vertices in CSR
-#pragma omp single
-            {
-                auto middle = high_resolution_clock::now();
-                log_info("before csr transform time: %.3lf s",
-                         duration_cast<milliseconds>(middle - start).count() / 1000.0);
-            }
+            // Scatter.
 #pragma omp for
             for (uint32_t i = 0; i < num_edges; i++) {
                 auto src = edge_lst[i].first;
                 auto dst = edge_lst[i].second;
 
-                uint32_t new_offset, old_offset;
-                do {
-                    old_offset = cur_write_off[src];
-                    new_offset = old_offset + 1;
-                } while (!__sync_bool_compare_and_swap(&(cur_write_off[src]), old_offset, new_offset));
+                auto old_offset = __sync_fetch_and_add(&(cur_write_off[src]), 1);
                 adj_lst[old_offset] = dst;
 
-                do {
-                    old_offset = cur_write_off[dst];
-                    new_offset = old_offset + 1;
-                } while (!__sync_bool_compare_and_swap(&(cur_write_off[dst]), old_offset, new_offset));
+                old_offset = __sync_fetch_and_add(&(cur_write_off[dst]), 1);
                 adj_lst[old_offset] = src;
             }
 #pragma omp single
             {
-                auto middle2 = high_resolution_clock::now();
-                log_info("before sort time: %.3lf s",
-                         duration_cast<milliseconds>(middle2 - start).count() / 1000.0);
+                log_info("before sort time: %.9lf s", convert_timer.elapsed());
             }
             // 5th: sort each local ranges
 #pragma omp for schedule(dynamic)
             for (auto u = 0; u < deg_lst.size(); u++) {
                 assert(cur_write_off[u] == off[u + 1]);
-                sort(begin(adj_lst) + off[u], begin(adj_lst) + off[u + 1]);
+                sort(adj_lst + off[u], adj_lst + off[u + 1]);
             }
         }
-        auto end = high_resolution_clock::now();
-        log_info("edge list to csr time: %.3lf s", duration_cast<milliseconds>(end - start).count() / 1000.0);
+        log_info("edge list to csr time: %.9lf s", convert_timer.elapsed());
 
         graph_t g;
-        g.n = off.size() - 1;
-        g.m = adj_lst.size();
-        g.adj = &adj_lst.front();
+        g.n = num_vertices;
+        assert(off[off.size() - 1] == 2 * num_edges);
+        g.m = off[off.size() - 1];
+        g.adj = adj_lst;
         g.num_edges = &off.front();
         size_t tc_cnt = 0;
 #pragma omp parallel for schedule(dynamic, 6000) reduction(+:tc_cnt)
